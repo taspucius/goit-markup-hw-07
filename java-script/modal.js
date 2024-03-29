bc27fc067a9584c5a26232fff665f237dca1001f
taspucius <158600654+taspucius@users.noreply.github.com>
2024-03-29 19:47:36 +0200

    repaired

--- java-script/modal.js
+++ java-script/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    overlay: document.querySelector(".overlay")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,8 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.overlay.classList.toggle("is-hidden");
   }
 })();
+
+
